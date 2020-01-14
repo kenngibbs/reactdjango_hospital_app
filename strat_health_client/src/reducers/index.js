d162bab33753c2a4eea4707f52da0b06c8ae31b2
KennGibbs <269162+kenngibbs@users.noreply.github.com>
2020-01-14 00:00:29 -0600

    Deleting unused redux file.

--- strat_health_client/src/reducers/index.js
+++
@@ -1,8 +0,0 @@
-export default function strat_hospital_reducers(state = {}, action) {
-    switch (action.type) {
-        case 'SET_AUTH_USER':
-            return action.hospital_list;
-        default:
-            return state;
-    }
-}
